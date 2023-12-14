7e61b09fff3e1e35fa55331e5f3624b0bd84871a
puranium <puranium235@gmail.com>
2023-12-14 23:47:06 +0900

    test: AuthButton story 작성

--- frontend/src/components/common/AuthButton/AuthButton.stories.js
+++ frontend/src/components/common/AuthButton/AuthButton.stories.js
@@ -1,15 +1,31 @@
-export default {};
 import AuthButton from './AuthButton';
 
 export default {
   title: 'Common/AuthButton',
   component: AuthButton,
-  argTypes: {
-    size: { control: '' },
-    disabled: { control: ''},
-    children: { control: 'text'},
-    onClick: { control: ''},
-  }
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
 };
 
-export const Primary =
+export const Login = {
+  args: {
+    children: '로그인',
+  },
+};
+
+export const DuplicationCheck = {
+  args: {
+    children: '중복확인',
+    size: 'sm',
+  },
+};
+
+export const CheckDone = {
+  args: {
+    children: '확인완료',
+    size: 'sm',
+    disabled: true,
+  },
+};
