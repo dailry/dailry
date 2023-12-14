8c14d116febd7e7de7ffa90c7aa102228129fdec
puranium <63571947+puranium235@users.noreply.github.com>
2023-12-14 10:27:05 +0900

    Feat/fe/#36 (#41)

---
+++ frontend/src/styles/color.js
@@ -0,0 +1,40 @@
+export const BACKGROUND = Object.freeze({});
+
+export const TEXT = Object.freeze({
+  black: '#000000',
+  white: '#ffffff',
+  purple: '#6b50e2',
+  placeHolder: '#d9d9d9',
+  disabled: '#616161',
+  error: '#f85555',
+  valid: '#4db15d',
+});
+
+export const HOME = Object.freeze({
+  ring: '#d9d9d9',
+  menu1: '#ffb3ea',
+  menu2: '#fdbf9c',
+  menu3: '#ffdba4',
+  menu4: '#aeddff',
+});
+
+export const FORM = Object.freeze({
+  input: '#9d9d9d',
+  inputClick: '#616161',
+  inputDisabled: '#c0c0c0',
+  button: '#87a5eb',
+  buttonClick: '#87a5eb',
+  buttonDisabled: '#9d9d9d',
+});
+
+// 다일리 왼쪽 메뉴
+// 닉네임은 TEXT.white, 그 밑의 아이디와 새 다일리는 text
+export const MENU = Object.freeze({
+  text: 'rgba(0, 0, 0, 0.4)',
+  line: '#ab7aa8',
+  title: '#303030',
+  box: 'rgba(0, 0, 0, 0)',
+  boxMouseOver: 'rgba(0, 0, 0, 0.1)',
+  boxCurrent: '#ffffff',
+  tool: '#74abd9',
+});
