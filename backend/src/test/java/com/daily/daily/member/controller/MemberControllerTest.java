f4381d20e5033a40d07268748cf890f4da0e8540
gomudayya <129571789+gomudayya@users.noreply.github.com>
2023-12-19 23:55:36 +0900

    remove : 임시 테스트코드 삭제

--- backend/src/test/java/com/daily/daily/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/daily/daily/member/controller/MemberControllerTest.java
@@ -8,13 +8,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberControllerTest {
 
-    @Test
-    @DisplayName("한글 사이즈를 3, 영어숫자 사이즈를 2라고 보았을 때 전체 사이즈가 30사이즈를 넘어가면 안된다.")
-
-    void join() {
-        JoinDTO joinDTO = new JoinDTO();
-        joinDTO.setNickname("ㅁㅁㅁㅁ@@");
-
-
-    }
 }
