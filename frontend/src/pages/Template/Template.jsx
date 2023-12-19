3cd00b0b4bd40e7bee16d9925e42873f06729755
puranium <puranium235@gmail.com>
2023-12-19 18:27:23 +0900

    feat: 배경 Template 컴포넌트 작성

--- frontend/src/pages/Template/Template.jsx
+++ frontend/src/pages/Template/Template.jsx
@@ -1,7 +1,12 @@
+import PropTypes from 'prop-types';
 import { Container } from './Template.styled';
 
 const Template = ({ children }) => {
   return <Template>{children}</Template>;
+};
+
+Template.PropTypes = {
+  children: PropTypes.elementType,
 };
 
 export default Template;
