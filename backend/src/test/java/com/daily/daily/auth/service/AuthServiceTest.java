540f3bc1579699546d1c7581dd0390d4969a4c23
SSUHYUNKIM <yellosuhyun@naver.com>
2023-12-23 03:03:10 +0900

    feat: AuthService 테스트 코드 작성

---
+++ backend/src/test/java/com/daily/daily/auth/service/AuthServiceTest.java
@@ -0,0 +1,58 @@
+package com.daily.daily.auth.service;
+
+import com.daily.daily.auth.dto.LoginDto;
+import com.daily.daily.member.controller.MemberController;
+import com.daily.daily.member.dto.JoinDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class AuthServiceTest {
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    MemberController memberController;
+
+    @BeforeEach
+    void join() {
+        JoinDTO joinDTO = new JoinDTO("testtset", "test1234", "난폭한사자");
+        memberController.join(joinDTO);
+        System.out.println("join");
+    }
+
+    @Test
+    @DisplayName("loginDTO의 정보를 받아 회원가입된 정보와 일치할 경우 로그인을 진행한다.")
+    void login() {
+
+        //given
+        String username = "testtset";
+        String password = "test1234";
+
+        //when
+        LoginDto loginDto = new LoginDto();
+        loginDto.setUsername(username);
+        loginDto.setPassword(password);
+
+        //then
+        try {
+            authService.login(loginDto);
+            System.out.println("로그인 성공!");
+        } catch (BadCredentialsException e) {
+            System.out.println("일치하는 password가 존재하지 않습니다.");
+        } catch (InternalAuthenticationServiceException e) {
+            System.out.println("일치하는 username이 존재하지 않습니다.");
+        }
+    }
+}
