5046c78fd5805f79a6f0b95ba08d365234687b36
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-12-26 21:26:49 +0900

    feat:DefaultLayout, DefaultNavigation 추가

---
+++ frontend/src/components/common/Navigation/DefaultNavigation.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const DefaultNavigation = () => {
+  return <>this is navigation</>;
+};
+
+export default DefaultNavigation;
+
+DefaultNavigation.propTypes = {
+  children: PropTypes.node,
+};
