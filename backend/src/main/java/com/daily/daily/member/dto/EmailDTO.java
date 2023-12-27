0ee9173f079d128b2c56d56ae93946b763a7680e
gomudayya <gomudayya@gmail.com>
2023-12-28 00:12:42 +0900

    feat : #84 이메일 인증번호 전송 기능 구현

--- backend/src/main/java/com/daily/daily/member/dto/EmailDTO.java
+++ backend/src/main/java/com/daily/daily/member/dto/EmailDTO.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmailDTO {
     @NotBlank
     @Email
--- backend/src/main/java/com/daily/daily/member/service/MemberEmailService.java
+++ backend/src/main/java/com/daily/daily/member/service/MemberEmailService.java
@@ -1,13 +1,31 @@
 package com.daily.daily.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
 public class MemberEmailService {
-    
-    public void sendCertificationNumber(String email) {
+    @Value("${spring.mail.username}")
+    private String senderEmail;
 
+    private final MailSender mailSender;
+
+    public void sendCertificationNumber(String recipientEmail) {
+        int randomNumber = ThreadLocalRandom.current().nextInt(100000, 1000000); // 6자리 인증 번호 생성
+
+        SimpleMailMessage mail = new SimpleMailMessage();
+
+        mail.setFrom(senderEmail);
+        mail.setTo(recipientEmail);
+        mail.setSubject("[다일리] 이메일 인증번호 입니다.");
+        mail.setText(String.format("이메일 인증번호는 %d 입니다.", randomNumber));
+
+        mailSender.send(mail);
     }
 }
