fd3ceed38f8833e48853a411eb12ffbc5bca367a
puranium <puranium235@gmail.com>
2023-12-28 22:07:42 +0900

    chore: storybook 및 의존성 버전 수정 등

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,17 +1,18 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
     autodocs: 'tag',
   },
+  staticDirs: ['../public'],
 };
 export default config;
