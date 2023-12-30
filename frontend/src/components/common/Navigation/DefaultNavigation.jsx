8dc08f9ee3fb67e19a0b5d03272f88cc6d63574a
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-12-30 21:16:03 +0900

    feat:NavigationItem 컴포넌트 추가 (#89)
    * NavigationItem.jsx, NavigationItem.styled.jsx 추가
    * 아이콘 에셋 추가
       - navigation-item-icon.svg
    * assets/svg 디렉토리 생성

--- frontend/src/components/common/Navigation/DefaultNavigation.jsx
+++ frontend/src/components/common/Navigation/DefaultNavigation.jsx
@@ -1,7 +1,16 @@
 import PropTypes from 'prop-types';
+import NavigationItem from '../NavigationItem/NavigationItem';
+import { NavigationItemIcon } from '../../../assets/svg';
 
 const DefaultNavigation = () => {
-  return <>this is navigation</>;
+  return (
+    <>
+      <NavigationItem icon={<NavigationItemIcon />}>daily</NavigationItem>
+      <NavigationItem icon={<NavigationItemIcon />} current>
+        daily
+      </NavigationItem>
+    </>
+  );
 };
 
 export default DefaultNavigation;
