551a44510a374ca2b315a10a80f06abf54384cd7
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-12-31 23:15:10 +0900

    style:컨벤션 반영하여 import 문, css 수정

--- frontend/src/components/common/Hamburger/Hamburger.jsx
+++ frontend/src/components/common/Hamburger/Hamburger.jsx
@@ -1,26 +1,21 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-  HamburgerContainer,
-  HamburgerMenu,
-  HamburgerMoreButton,
-  Overlay,
-} from './Hamburger.styled';
+import * as S from './Hamburger.styled';
 
 const Hamburger = (props) => {
   const { anchor = 'right', children } = props;
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <HamburgerContainer>
-      <HamburgerMoreButton
+    <S.HamburgerContainer>
+      <S.HamburgerMoreButton
         onClick={() => {
           setIsOpen((prev) => !prev);
         }}
       />
-      {isOpen && <Overlay onClick={() => setIsOpen(false)} />}
-      {isOpen && <HamburgerMenu anchor={anchor}>{children}</HamburgerMenu>}
-    </HamburgerContainer>
+      {isOpen && <S.Overlay onClick={() => setIsOpen(false)} />}
+      {isOpen && <S.HamburgerMenu anchor={anchor}>{children}</S.HamburgerMenu>}
+    </S.HamburgerContainer>
   );
 };
 
--- frontend/src/components/common/Hamburger/Hamburger.styled.jsx
+++ frontend/src/components/common/Hamburger/Hamburger.styled.jsx
@@ -12,38 +12,44 @@ const fadeIn = keyframes`
 
 export const HamburgerContainer = styled.div`
   position: relative;
+
   width: fit-content;
 `;
 
 export const HamburgerMenu = styled.div`
-  position: absolute;
-  z-index: 2;
-  width: auto;
-  top: 30px;
-  right: ${({ anchor }) => (anchor === 'left' ? '0px' : 'auto')};
-  left: ${({ anchor }) => (anchor === 'right' ? '0px' : 'auto')};
   display: flex;
   flex-direction: column;
   align-items: center;
   row-gap: 8px;
+  position: absolute;
+  top: 30px;
+  right: ${({ anchor }) => (anchor === 'left' ? '0px' : 'auto')};
+  left: ${({ anchor }) => (anchor === 'right' ? '0px' : 'auto')};
+  z-index: 2;
+
+  width: auto;
+  padding: 8px 16px 8px 8px;
+
   border-radius: 10px;
   background: #fff;
+
   box-shadow:
     4px -4px 10px 0px rgba(0, 0, 0, 0.25),
     -4px 4px 10px 0px rgba(0, 0, 0, 0.25);
-  padding: 8px 16px 8px 8px;
   animation: ${fadeIn} 0.3s;
 `;
 
 export const HamburgerItem = styled.div`
   display: flex;
-  column-gap: 8px;
-  width: 100%;
-  white-space: nowrap;
   align-items: center;
+  column-gap: 8px;
+
+  width: 100%;
 
   font-size: 11px;
+
   cursor: pointer;
+  white-space: nowrap;
 `;
 
 export const HamburgerMoreButton = styled(HamburgerMoreIcon)`
@@ -57,9 +63,10 @@ export const HamburgerMoreButton = styled(HamburgerMoreIcon)`
 
 export const Overlay = styled.div`
   position: absolute;
-  z-index: 1;
   top: 0px;
   left: 0px;
+  z-index: 1;
+
   width: 100vw;
   height: 100vh;
 `;
