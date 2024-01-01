fdffc9701fb5bc2d3c318bf06782ecbe415c3d81
gomudayya <129571789+gomudayya@users.noreply.github.com>
2024-01-01 18:22:09 +0900

    submodule 파일이 복사가 안되는 문제 해결 (#98)
    * fix : #97 build.gradle 수정
    
    * rename : #97 서브모듈 업데이트. datasource의 url 오타정정
    
    * refacto : #97 필요없는 스크립트 정리
    
    * refacto : #97 ExceptionResponseDTO 필드 수정

--- backend/src/main/java/com/daily/daily/common/dto/ExceptionResponseDTO.java
+++ backend/src/main/java/com/daily/daily/common/dto/ExceptionResponseDTO.java
@@ -10,6 +10,12 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ExceptionResponseDTO {
+    private boolean isSuccessful = false;
     private String msg;
     private Integer statusCode;
+
+    public ExceptionResponseDTO(String msg, Integer statusCode) {
+        this.msg = msg;
+        this.statusCode = statusCode;
+    }
 }
