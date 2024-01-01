c3bc1c39b774d6389256dcf1cf58e4df0f06ac55
SSUHYUNKIM <yellosuhyun@naver.com>
2024-01-01 23:28:48 +0900

    refactor : 코드 리뷰 반영 수정

--- backend/src/main/java/com/daily/daily/auth/controller/AuthController.java
+++ backend/src/main/java/com/daily/daily/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.daily.daily.auth.jwt.JwtUtil;
 import com.daily.daily.auth.jwt.RefreshToken;
 import com.daily.daily.auth.service.AuthService;
 import com.daily.daily.auth.service.TokenService;
+import com.daily.daily.common.dto.CommonResponseDTO;
 import com.daily.daily.common.dto.ExceptionResponseDTO;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -25,11 +26,11 @@ public class AuthController {
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<ExceptionResponseDTO> login(@RequestBody LoginDTO loginDto, HttpServletResponse response) {
+    public ResponseEntity<CommonResponseDTO> login(@RequestBody LoginDTO loginDto, HttpServletResponse response) {
         TokenDTO tokenDto = authService.login(loginDto);
         response.setHeader(jwtUtil.getAccessHeader(), tokenDto.getAccessToken());
         response.setHeader(jwtUtil.getRefreshHeader(), tokenDto.getRefreshToken());
-        return ResponseEntity.ok().body(new ExceptionResponseDTO("로그인 성공", HttpStatus.OK.value()));
+        return ResponseEntity.ok().body(new CommonResponseDTO(true, HttpStatus.OK.value()));
     }
 
     @PostMapping("/token")
