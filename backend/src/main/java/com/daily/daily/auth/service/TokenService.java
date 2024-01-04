c7e58e04f4e7733fe01ac6983a949e69fc04ad7a
SSUHYUNKIM <yellosuhyun@naver.com>
2024-01-04 19:15:01 +0900

    feat : 인증 & 인가 API restdocs 작성

--- backend/src/main/java/com/daily/daily/auth/service/TokenService.java
+++ backend/src/main/java/com/daily/daily/auth/service/TokenService.java
@@ -4,15 +4,14 @@ import com.daily.daily.auth.jwt.JwtUtil;
 import com.daily.daily.auth.jwt.RefreshToken;
 import com.daily.daily.auth.jwt.RefreshTokenRepository;
 import com.daily.daily.member.domain.Member;
+import com.daily.daily.member.exception.MemberNotFoundException;
 import com.daily.daily.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 @RequiredArgsConstructor
-@Transactional
 @Service
 public class TokenService {
     private final RefreshTokenRepository refreshTokenRepository;
@@ -24,9 +23,8 @@ public class TokenService {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         Member member = memberRepository.findById(refreshToken1.getId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(MemberNotFoundException::new);
 
-
-        return jwtUtil.generateAccessToken(member.getId(), member.getRole());
+        return jwtUtil.generateAccessToken(refreshToken1.getId(), member.getRole());
     }
 }
