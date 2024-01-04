484d893e41c61d044a931d096f42a1b8f306942f
gomudayya <gomudayya@gmail.com>
2024-01-04 18:05:28 +0900

    chore : #110 cors 세팅 변경

--- backend/src/main/java/com/daily/daily/auth/config/CorsConfig.java
+++ backend/src/main/java/com/daily/daily/auth/config/CorsConfig.java
@@ -3,8 +3,11 @@ package com.daily.daily.auth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -20,5 +23,21 @@ public class CorsConfig {
         source.registerCorsConfiguration("/api/**", config);
 
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
