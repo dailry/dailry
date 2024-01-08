7301a3a4b30845ecd6008f30386f59cff4f9a387
SSUHYUNKIM <yellosuhyun@naver.com>
2024-01-08 12:42:02 +0900

    refactor : 코드 리뷰 반영

--- backend/src/main/java/com/daily/daily/auth/jwt/LoginTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.daily.daily.auth.jwt;
-
-import com.daily.daily.auth.dto.LoginTokenDTO;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LoginTokenRepository extends CrudRepository<LoginTokenDTO,String> {
-}
