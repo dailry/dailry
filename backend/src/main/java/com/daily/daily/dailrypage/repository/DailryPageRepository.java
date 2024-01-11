6a078b40931f89d64d9b970ecb16290bd8db6464
gomudayya <129571789+gomudayya@users.noreply.github.com>
2024-01-11 13:26:19 +0900

    다일리 페이지 관련 기능 구현 (#133)
    * chore : JPA로 Json 타입을 다루기 위해 hibernate util 의존성 추가
    
    * feat : #129 다일리 생성및 수정기능 구현
    
    * feat : #129 다일리 페이지 조회, 삭제 기능 구현

---
+++ backend/src/main/java/com/daily/daily/dailrypage/repository/DailryPageRepository.java
@@ -0,0 +1,7 @@
+package com.daily.daily.dailrypage.repository;
+
+import com.daily.daily.dailrypage.domain.DailryPage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DailryPageRepository extends JpaRepository<DailryPage, Long> {
+}
