f0481fea08390529097fd197532e93c8c358bf79
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-01-17 16:36:23 +0900

    chore:불필요한 코드 제거
    * 이미지로 변환하기 기능 제거

--- frontend/src/components/decorate/Drawing/Drawing.jsx
+++ frontend/src/components/decorate/Drawing/Drawing.jsx
@@ -1,6 +1,5 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import PropTypes from 'prop-types';
-import { canvasToImage } from './canvas';
 import useDrawUtils from './useDrawUtils';
 import AuthButton from '../../common/AuthButton/AuthButton';
 import useDrawInstance from './useDrawInstance';
@@ -19,21 +18,17 @@ const Drawing = (props) => {
     setMode,
   } = useDrawUtils(canvas, drawInstance);
 
-  const [imgSrc, setImgSrc] = useState('');
-
   return (
     <>
       <canvas
+        id={id}
         onMouseDown={(e) => startMoving(e, mouseEventHandlers[mode])}
         onMouseUp={() => stopMoving(mouseEventHandlers[mode])}
         ref={canvas}
       ></canvas>
-      <image id="canvas-draw-image" alt="converted-image" src={imgSrc} />
-      <AuthButton onClick={() => setImgSrc(canvasToImage(id))}>
-        이미지로 변환하기
-      </AuthButton>
 
-      <AuthButton onClick={() => saveCanvas()}>저장</AuthButton>
+      <AuthButton onClick={() => saveCanvas()}>저장하기</AuthButton>
+
       <AuthButton onClick={() => setMode('drawMode')}>그리기</AuthButton>
 
       <AuthButton onClick={() => setMode('eraseMode')}>지우기</AuthButton>
--- frontend/src/components/decorate/Drawing/canvas.js
+++ frontend/src/components/decorate/Drawing/canvas.js
@@ -8,22 +8,3 @@ export const createCtx = (canvas) => {
 
   return { ctx };
 };
-
-export const canvasToImage = (id) => {
-  const canvas = document.getElementById(id);
-  const imgBase64 = canvas.toDataURL('image/jpeg', 'image/octet-stream');
-  const decodeImg = atob(imgBase64.split(',')[1]);
-
-  const array = [];
-  for (let i = 0; i < decodeImg.length; i += 1) {
-    array.push(decodeImg.charCodeAt(i));
-  }
-
-  const file = new Blob([new Uint8Array(array)], { type: 'image/jpeg' });
-  const fileName = `canvas_img_${new Date().getMilliseconds()}.jpg`;
-  const formData = new FormData();
-
-  formData.append('file_give', file, fileName);
-
-  return imgBase64;
-};
