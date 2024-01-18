becb74ebe307e8f77487be44fdd74f109072c992
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-01-18 23:30:33 +0900

    feat:꾸미기 컴포넌트 생성 기능
    * useCreateDecorateComponent 커스텀 훅 생성
       - 꾸미기 컴포넌트 들이 담겨있는 배열에 삽입/삭제 하는 방식으로 구현.
       - type 관련하여 추가 구현 필요.
    * jsx-a11y/no-static-element-interactions 규칙 제거

--- frontend/src/pages/DailryPage/DailryPage.jsx
+++ frontend/src/pages/DailryPage/DailryPage.jsx
@@ -1,14 +1,30 @@
 // Da-ily 회원, 비회원, 다일리 있을때, 없을때를 조건문으로 나눠서 렌더링
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Moveable from '../../components/da-ily/Moveable/Moveable';
 import Drawing from '../../components/decorate/Drawing/Drawing';
 import * as S from './DailryPage.styled';
 import dailryData from './dailry.json';
+import Button from '../../components/common/Button/Button';
+import useCreateDecorateComponent from '../../hooks/useCreateDecorateComponent';
+
+const DecorateComponents = {
+  drawing: {
+    type: 'drawing',
+    component: <Drawing />,
+  },
+};
 
 const DailryPage = () => {
   const { elements } = dailryData;
   const [target, setTarget] = useState(null);
   const moveableRef = useRef(null);
+  const [decorateComponents, setDecorateComponents] = useState([]);
+  const { setNewDecorateComponentPosition, createNewDecorateComponent } =
+    useCreateDecorateComponent(setDecorateComponents);
+
+  useEffect(() => {
+    // if (elements) setDecorateComponents(elements);
+  }, [elements]);
 
   const handleMouseDown = (e) => {
     const { nativeEvent } = e;
@@ -19,9 +35,9 @@ const DailryPage = () => {
   };
   return (
     <S.FlexWrapper>
-      <S.CanvasWrapper>
+      <S.CanvasWrapper onClick={(e) => setNewDecorateComponentPosition(e)}>
         <Drawing id="first" />
-        {elements.map((element) => {
+        {decorateComponents.map((element) => {
           const { id, type, position, properties } = element;
           return (
             <div
@@ -29,13 +45,16 @@ const DailryPage = () => {
               onMouseDown={handleMouseDown}
               style={S.ElementStyle({ position, properties })}
             >
-              {type}
+              {DecorateComponents[type].component}
             </div>
           );
         })}
-        <Moveable target={target} moveableRef={moveableRef} />
+        {target && <Moveable target={target} moveableRef={moveableRef} />}
       </S.CanvasWrapper>
       <S.ToolWrapper>
+        <Button onClick={() => createNewDecorateComponent('drawing')}>
+          드로잉 컴포넌트 생성
+        </Button>
         <div>tool1크기</div>
         <div>tool2</div>
       </S.ToolWrapper>
