1bb3867c70a456871c618318684f91e1d0953531
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-01-21 23:10:21 +0900

    꾸미기 컴포넌트 생성 (#166)
    * feat:꾸미기 컴포넌트 생성 기능
    
    * useCreateDecorateComponent 커스텀 훅 생성
       - 꾸미기 컴포넌트 들이 담겨있는 배열에 삽입/삭제 하는 방식으로 구현.
       - type 관련하여 추가 구현 필요.
    * jsx-a11y/no-static-element-interactions 규칙 제거
    
    * merge:develop 브랜치 merge
    
    * https://github.com/dailry/dailry/issues/163 변경사항에 맞게 적용

--- frontend/src/pages/DailryPage/DailryPage.jsx
+++ frontend/src/pages/DailryPage/DailryPage.jsx
@@ -1,22 +1,44 @@
 // Da-ily 회원, 비회원, 다일리 있을때, 없을때를 조건문으로 나눠서 렌더링
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import Moveable from '../../components/da-ily/Moveable/Moveable';
-
+import Drawing from '../../components/decorate/Drawing/Drawing';
 import * as S from './DailryPage.styled';
 import dailryData from './dailry.json';
+import Button from '../../components/common/Button/Button';
+import useCreateDecorateComponent from '../../hooks/useCreateDecorateComponent';
+
+const TextBox = () => <div>ff</div>;
+
+const DecorateComponents = {
+  drawing: {
+    type: 'drawing',
+    component: <Drawing />,
+  },
+  textbox: {
+    component: <TextBox />,
+  },
+};
 
 const DailryPage = () => {
   const { elements } = dailryData;
   const [target, setTarget] = useState(null);
+
   const moveableRef = useRef([]);
+  const [decorateComponents, setDecorateComponents] = useState([]);
+  const { setNewDecorateComponentPosition, createNewDecorateComponent } =
+    useCreateDecorateComponent(setDecorateComponents);
+
+  useEffect(() => {
+    if (elements) setDecorateComponents(elements);
+  }, [elements]);
 
   const handleMouseDown = (e, index) => {
-    setTarget(index);
+    setTarget(index + 1);
   };
   return (
     <S.FlexWrapper>
-      <S.CanvasWrapper>
-        {elements.map((element, index) => {
+      <S.CanvasWrapper onClick={(e) => setNewDecorateComponentPosition(e)}>
+        {decorateComponents.map((element, index) => {
           const { id, type, position, properties } = element;
           return (
             <div
@@ -24,17 +46,22 @@ const DailryPage = () => {
               onMouseDown={(e) => handleMouseDown(e, index)}
               style={S.ElementStyle({ position, properties })}
               ref={(el) => {
-                moveableRef[index] = el;
+                moveableRef[index + 1] = el;
               }}
             >
-              {type}
-              <div>child element</div>
+              {DecorateComponents[type].component}
             </div>
           );
         })}
         {target && <Moveable target={moveableRef[target]} />}
       </S.CanvasWrapper>
       <S.ToolWrapper>
+        <Button onClick={() => createNewDecorateComponent('drawing')}>
+          드로잉 컴포넌트 생성
+        </Button>
+        <Button onClick={() => createNewDecorateComponent('textbox')}>
+          텍스트 박스 컴포넌트 생성
+        </Button>
         <div>tool1크기</div>
         <div>tool2</div>
       </S.ToolWrapper>
