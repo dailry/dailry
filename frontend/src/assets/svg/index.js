89802f446d10575862c3dc8e60396b03e4818e8e
puranium <puranium235@gmail.com>
2024-01-22 13:08:46 +0900

    fix: 오타수정

--- frontend/src/assets/svg/index.js
+++ frontend/src/assets/svg/index.js
@@ -4,5 +4,5 @@ export { ReactComponent as NavigationItemIcon } from './navigation-item-icon.svg
 export { ReactComponent as RingIcon } from './ring-icon.svg';
 export { ReactComponent as CursorIcon } from './cursor-icon.svg';
 export { ReactComponent as DrawIcon } from './draw-icon.svg';
-export { ReactComponet as TextIcon } from './text-icon.svg';
+export { ReactComponent as TextIcon } from './text-icon.svg';
 export { ReactComponent as StickerIcon } from './sticker-icon.svg';
