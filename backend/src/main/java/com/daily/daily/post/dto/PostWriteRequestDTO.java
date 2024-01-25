4ecf7f0af001fab2aa57495a3e2d5478cdaea7e1
gomudayya <gomudayya@gmail.com>
2024-01-26 00:04:58 +0900

    feat : #189 한 게시글에 붙일 수 있는 해시태그를 최대 5개로 검증로직 추가

--- backend/src/main/java/com/daily/daily/post/dto/PostWriteRequestDTO.java
+++ backend/src/main/java/com/daily/daily/post/dto/PostWriteRequestDTO.java
@@ -1,22 +1,23 @@
 package com.daily.daily.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Set;
 
-@Setter
 @Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostWriteRequestDTO {
-    @NotBlank(message = "content 필드는 반드시 존재해야 합니다.")
+    @NotNull(message = "content 필드는 반드시 존재해야 합니다.")
     private String content;
 
+    @Size(max = 5, message = "게시글에 붙일 수 있는 해시태그는 최대 5개 입니다.")
     private Set<String> hashtags;
 }
