6b4031a33788d1c7fdc50ec121429f8767f6aa09
SSUHYUNKIM <yellosuhyun@naver.com>
2024-01-25 15:21:11 +0900

    feat : 다일리 전체 리스트 조회

--- backend/src/main/java/com/daily/daily/dailry/controller/DailryController.java
+++ backend/src/main/java/com/daily/daily/dailry/controller/DailryController.java
@@ -2,6 +2,7 @@ package com.daily.daily.dailry.controller;
 
 import com.daily.daily.common.dto.SuccessResponseDTO;
 import com.daily.daily.dailry.dto.DailryDTO;
+import com.daily.daily.dailry.dto.DailryFindDTO;
 import com.daily.daily.dailry.dto.DailryUpdateDTO;
 import com.daily.daily.dailry.service.DailryService;
 import jakarta.validation.Valid;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -25,18 +28,23 @@ public class DailryController {
     }
 
     @PatchMapping("/{dailryId}")
-    public DailryDTO updateDailry(@RequestBody DailryUpdateDTO dailryUpdateDTO, @PathVariable Long dailryId) {
-        return dailryService.update(dailryId, dailryUpdateDTO);
+    public DailryDTO updateDailry(@AuthenticationPrincipal Long memberId, @RequestBody DailryUpdateDTO dailryUpdateDTO, @PathVariable Long dailryId) {
+        return dailryService.update(memberId, dailryId, dailryUpdateDTO);
     }
 
     @GetMapping("/{dailryId}")
-    public DailryDTO findDailry(@PathVariable Long dailryId) {
-        return dailryService.find(dailryId);
+    public DailryDTO findDailry(@AuthenticationPrincipal Long memberId, @PathVariable Long dailryId) {
+        return dailryService.find(memberId, dailryId);
+    }
+
+    @GetMapping
+    public List<DailryFindDTO> findAllDailry(@AuthenticationPrincipal Long memberId) {
+        return dailryService.findAll(memberId);
     }
 
     @DeleteMapping("/{dailryId}")
-    public SuccessResponseDTO deleteDailry(@PathVariable Long dailryId) {
-        dailryService.delete(dailryId);
+    public SuccessResponseDTO deleteDailry(@AuthenticationPrincipal Long memberId, @PathVariable Long dailryId) {
+        dailryService.delete(memberId, dailryId);
         return new SuccessResponseDTO(true, HttpStatus.OK.value());
     }
 }
---
+++ backend/src/main/java/com/daily/daily/dailry/dto/DailryFindDTO.java
@@ -0,0 +1,13 @@
+package com.daily.daily.dailry.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DailryFindDTO {
+    private Long id;
+    private String title;
+}
