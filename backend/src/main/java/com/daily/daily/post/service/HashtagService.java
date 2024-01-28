aaa34630905a954644483801b5d3f5021265e337
gomudayya <gomudayya@gmail.com>
2024-01-28 23:28:12 +0900

    refactor : #189 updateHashtagsInPost 주석 작성

--- backend/src/main/java/com/daily/daily/post/service/HashtagService.java
+++ backend/src/main/java/com/daily/daily/post/service/HashtagService.java
@@ -40,6 +40,13 @@ public class HashtagService {
     }
 
     public void updateHashtagsInPost(Post post, Set<String> hashtags) {
+        /**
+         * 현재 코드는 기존 post가 가지고 있는 해시태그의 갯수만큼 delete 쿼리가 발생한다.
+         * 쿼리를 최소화 하고싶으면, 게시글에 있는 해시태그가 수정목록에 존재하지 않으면 목록에서 지우고, 꼭 추가해야만 하는 해시태그만 add 해주면 된다.
+         * 대신 이러면 코드가 15줄은 더 늘어난다.ㅠ ㅠ
+         */
+
+
         post.clearHashtags();
         addHashtagsToPost(post, hashtags);
     }
