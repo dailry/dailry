f49f05478eee2946771894caad0d45b36adc46db
SSUHYUNKIM <yellosuhyun@naver.com>
2024-01-31 16:29:41 +0900

    feat : 섬네일 저장 기능 구현

--- backend/src/main/java/com/daily/daily/dailrypage/controller/DailryPageController.java
+++ backend/src/main/java/com/daily/daily/dailrypage/controller/DailryPageController.java
@@ -8,10 +8,13 @@ import com.daily.daily.dailrypage.dto.DailryPageThumbnailDTO;
 import com.daily.daily.dailrypage.dto.DailryPageUpdateDTO;
 import com.daily.daily.dailrypage.service.DailryPageService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -24,33 +27,43 @@ public class DailryPageController {
     private final DailryPageService dailryPageService;
 
     @PostMapping("/api/pages")
+    @Secured(value = "ROLE_MEMBER")
     @ResponseStatus(HttpStatus.CREATED)
     public DailryPageCreateResponseDTO createPage() {
         return dailryPageService.create();
     }
 
     @PostMapping("/api/dailry/{dailryID}/pages")
+    @Secured(value = "ROLE_MEMBER")
     @ResponseStatus(HttpStatus.CREATED)
     public DailryPageDTO createPage2(@AuthenticationPrincipal Long memberId, @PathVariable Long dailryID) {
         return dailryPageService.create2(memberId, dailryID);
     }
 
-    @PatchMapping("/api/pages/{pageId}")
-    public DailryPageDTO updatePage(@AuthenticationPrincipal Long memberId, @RequestBody @Valid DailryPageUpdateDTO dailryPageUpdateDTO, @PathVariable Long pageId) {
-        return dailryPageService.update(memberId, pageId, dailryPageUpdateDTO);
+    @PostMapping("/api/pages/{pageId}")
+    @Secured(value = "ROLE_MEMBER")
+    public DailryPageDTO updatePage(
+            @AuthenticationPrincipal Long memberId,
+            @PathVariable Long pageId,
+            @RequestPart @Valid DailryPageUpdateDTO dailryPageRequest,
+            @RequestPart @NotNull MultipartFile thumbnail) {
+        return dailryPageService.update(memberId, pageId, dailryPageRequest, thumbnail);
     }
 
     @GetMapping("/api/pages/{pageId}")
+    @Secured(value = "ROLE_MEMBER")
     public DailryPageDTO findPage(@AuthenticationPrincipal Long memberId, @PathVariable Long pageId) {
         return dailryPageService.find(memberId, pageId);
     }
 
     @GetMapping("/api/dailry/{dailryId}/pages")
+    @Secured(value = "ROLE_MEMBER")
     public DailryPagePreviewDTO findAllPage(@AuthenticationPrincipal Long memberId, @PathVariable Long dailryId) {
         return dailryPageService.findAll(memberId, dailryId);
     }
 
     @DeleteMapping("/api/pages/{pageId}")
+    @Secured(value = "ROLE_MEMBER")
     public SuccessResponseDTO deletePage(@AuthenticationPrincipal Long memberId, @PathVariable Long pageId) {
         dailryPageService.delete(memberId, pageId);
         return new SuccessResponseDTO(true, HttpStatus.OK.value());
