31a5e04140fe3c20916b0d877d597ce961552a10
puranium <puranium235@gmail.com>
2024-02-05 20:40:29 +0900

    fix: 연속으로 삭제하면 리렌더링이 안되는 몬제 해결

--- frontend/src/components/common/Hamburger/DailryHamburger.jsx
+++ frontend/src/components/common/Hamburger/DailryHamburger.jsx
@@ -5,7 +5,7 @@ import { deleteDailry } from '../../../apis/dailryApi';
 const DailryHamburger = (props) => {
   const { dailryId, setEditingDailry, anchor = 'left' } = props;
   const handleDeleteClick = () => {
-    deleteDailry(dailryId).then(setEditingDailry(0));
+    deleteDailry(dailryId).then(setEditingDailry(Math.random()));
   };
 
   const handlePatchClick = () => {
