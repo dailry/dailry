2cd3cf0f86ccf51ad7d1d535cc3304269c109785
puranium <puranium235@gmail.com>
2024-02-16 15:00:34 +0900

    feat: 레이아웃 크기별로 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,10 +18,12 @@ const App = () => {
       <Route element={<RootLayout />}>
         <Route path={PATH_NAME.Dailry} element={<DailryPage />} />
         <Route path={PATH_NAME.MyPage} element={<MyPage />} />
+      </Route>
+      <Route element={<RootLayout templateSize={'half'} />}>
         <Route path={PATH_NAME.Community} element={<CommunityPage />} />
         <Route path={PATH_NAME.AdminMembers} element={<AdminMembersPage />} />
       </Route>
-      <Route element={<RootLayout hasTemplate={true} />}>
+      <Route element={<RootLayout templateSize={'full'} />}>
         <Route path={PATH_NAME.Login} element={<LoginPage />} />
         <Route path={PATH_NAME.FindId} element={<FindIdPage />} />
         <Route path={PATH_NAME.Join} element={<JoinPage />} />
