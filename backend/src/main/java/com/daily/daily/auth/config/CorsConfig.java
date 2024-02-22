cdb85072f49b789480aab47e0d689750ce0d584c
gomudayya <129571789+gomudayya@users.noreply.github.com>
2024-02-22 16:05:04 +0900

    CorsConfig 에서 Profile 별로 설정 빈을 다르게 등록하기 (#246)
    * refactor : #243 CorsConfig 에서 Profile 별로 설정 빈을 다르게 등록하기
    
    * refactor : #245 corsConfigurationSource 빈 이름 설정

--- backend/src/main/java/com/daily/daily/auth/config/CorsConfig.java
+++ backend/src/main/java/com/daily/daily/auth/config/CorsConfig.java
@@ -2,24 +2,43 @@ package com.daily.daily.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    @Bean(name = "corsConfigurationSource")
+    @Profile({"local", "dev"})
+    public CorsConfigurationSource localDevCorsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:3000", "http://localhost:3000", "https://localhost:3000", "https://da-ily.site"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean(name = "corsConfigurationSource")
+    @Profile({"prod"})
+    public CorsConfigurationSource prodCorsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of("https://dailry.co.kr"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+        configuration.setMaxAge(1800L); // preflight 요청을 30분 동안 캐싱. 불필요한 preflight 요청 감소
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
