6438641766b39f251f17bbffb8fffa7fc413ca60
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-02-27 16:46:38 +0900

    feat:moveable 이벤트 에 공통 속성 수정하는 함수 부착
    * onDragEnd, onRotateEnd, onResizeEnd

--- frontend/src/components/da-ily/Moveable/Moveable.jsx
+++ frontend/src/components/da-ily/Moveable/Moveable.jsx
@@ -4,7 +4,7 @@ import MoveableHelper from 'moveable-helper';
 import { useState } from 'react';
 
 const MoveableComponent = (props) => {
-  const { target } = props;
+  const { target, setCommonProperty } = props;
   const [helper] = useState(() => {
     return new MoveableHelper();
   });
@@ -20,15 +20,43 @@ const MoveableComponent = (props) => {
       throttleRotate={0}
       onDragStart={helper.onDragStart}
       onDrag={helper.onDrag}
+      onDragEnd={(e) => {
+        if (e.isDrag) {
+          const movedX = parseInt(e.lastEvent.translate[0], 10);
+          const movedY = parseInt(e.lastEvent.translate[1], 10);
+
+          setCommonProperty({
+            position: {
+              x: movedX,
+              y: movedY,
+            },
+          });
+        }
+      }}
       onRotateStart={helper.onRotateStart}
       onRotate={helper.onRotate}
+      onRotateEnd={(e) => {
+        const transformString = e.target.style.transform;
+        const deg = transformString.split('rotate(')[1].split('deg')[0];
+
+        setCommonProperty({ rotation: deg });
+      }}
       onResizeStart={helper.onResizeStart}
       onResize={helper.onResize}
+      onResizeEnd={(e) =>
+        setCommonProperty({
+          size: {
+            width: e.target.style.width,
+            height: e.target.style.height,
+          },
+        })
+      }
     />
   );
 };
 
 MoveableComponent.propTypes = {
   target: PropTypes.object,
+  setCommonProperty: PropTypes.func,
 };
 export default MoveableComponent;
