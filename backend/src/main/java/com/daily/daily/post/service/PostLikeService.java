c54b7de3c8cf6fdb9fea9a69161e8c3fe20e2f6e
gomudayya <gomudayya@gmail.com>
2024-03-01 14:29:41 +0900

    refactor : #250 낙관적 락을 이용해서 좋아요의 동시성 문제 해결

--- backend/src/main/java/com/daily/daily/post/service/PostLikeService.java
+++ backend/src/main/java/com/daily/daily/post/service/PostLikeService.java
@@ -6,13 +6,12 @@ import com.daily.daily.member.repository.MemberRepository;
 import com.daily.daily.post.domain.Post;
 import com.daily.daily.post.domain.PostLike;
 import com.daily.daily.post.exception.AlreadyLikeException;
-import com.daily.daily.post.exception.LikeDecreaseNotAllowedException;
+import com.daily.daily.post.exception.NotPreviouslyLikedException;
 import com.daily.daily.post.exception.PostNotFoundException;
 import com.daily.daily.post.repository.PostLikeRepository;
 import com.daily.daily.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -29,8 +28,10 @@ public class PostLikeService {
         }
 
         Member findMember = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
-//        Post findPost = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
-        Post findPost = postRepository.findWithPessimisticLock(postId).orElseThrow(PostNotFoundException::new);
+        Post findPost = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
+
+        findPost.increaseLikeCount();
+        postRepository.saveAndFlush(findPost); // 낙관적락 예외 발생 가능성 존재
 
         PostLike like = PostLike.builder()
                 .member(findMember)
@@ -38,15 +39,14 @@ public class PostLikeService {
                 .build();
 
         likeRepository.save(like);
-
-//        postRepository.increaseLikeCount(postId);
-        findPost.increaseLikeCount();
     }
 
     public void decreaseLikeCount(Long memberId, Long postId) {
-        PostLike like = likeRepository.findBy(memberId, postId)
-                .orElseThrow(LikeDecreaseNotAllowedException::new);
+        PostLike like = likeRepository.findBy(memberId, postId).orElseThrow(NotPreviouslyLikedException::new);
+        Post findPost = postRepository.findById(postId).orElseThrow(PostNotFoundException::new);
 
+        findPost.decreaseLikeCount();
+        postRepository.saveAndFlush(findPost); // 낙관적 락 예외 가능성 존재
         likeRepository.delete(like);
     }
 }
--- backend/src/test/java/com/daily/daily/post/service/PostLikeServiceConcurrencyTest.java
+++ backend/src/test/java/com/daily/daily/post/service/PostLikeServiceConcurrencyTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.C;
 
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -31,32 +32,60 @@ public class PostLikeServiceConcurrencyTest {
     PostRepository postRepository;
 
     @Autowired
-    PostLikeService postLikeService;
-
-
+    PostLikeFacade postLikeFacade;
     @Test
-    @DisplayName("동시에 좋아요 요청이 50개 왔을 때, 좋아요의 갯수가 50이 오르는지 확인한다.")
+    @DisplayName("동시에 좋아요 요청이 50개 왔을 때, 좋아요의 갯수가 50이 오르는지 확인한다. " +
+            "그리고 이후에 좋아요 해제 요청이 동시에 30개가 왔을 때 전체 좋아요의 갯수는 20이어야 한다.")
     void likeIncreaseTest() throws InterruptedException {
+        //테스트1 : 동시에 좋아요 요청이 50개 왔을 때, 좋아요의 갯수가 50이 오르는지 확인한다.
         //given
-        int concurrentRequestCount = 50; // 동시 요청 횟수
-        List<Member> members = memberGenerator.generate(concurrentRequestCount);
+        int likeIncreaseRequest = 50; // 동시 요청 횟수
+        List<Member> members = memberGenerator.generate(likeIncreaseRequest);
         Post post = postGenerator.generate();
 
         ExecutorService executorService = Executors.newFixedThreadPool(32);
-        CountDownLatch latch = new CountDownLatch(concurrentRequestCount);
+        CountDownLatch latch = new CountDownLatch(likeIncreaseRequest);
 
         //when
         for (Member member : members) {
             executorService.submit(() -> {
-                postLikeService.increaseLikeCount(member.getId(), post.getId());
-                latch.countDown();
+                try {
+                    postLikeFacade.increaseLikeCount(member.getId(), post.getId());
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             });
         }
 
         latch.await();
 
         //then
-        Post findPost = postRepository.findById(post.getId()).orElseThrow(PostNotFoundException::new);
-        assertThat(findPost.getLikeCount()).isEqualTo(concurrentRequestCount);
+        Post postWithIncreasedLikes = postRepository.findById(post.getId()).orElseThrow(PostNotFoundException::new);
+        assertThat(postWithIncreasedLikes.getLikeCount()).isEqualTo(likeIncreaseRequest);
+
+        //테스트 2 : 이후에 좋아요 해제 요청이 동시에 30개가 왔을 때 전체 좋아요의 갯수는 20이어야 한다.
+        //given
+        int likeDecreaseRequest = 30;
+        CountDownLatch latch2 = new CountDownLatch(likeDecreaseRequest);
+        List<Member> likeDecreaseMembers = members.subList(0, 30);
+
+        //when
+        for (Member member : likeDecreaseMembers) {
+            executorService.submit(() -> {
+                try {
+                    postLikeFacade.decreaseLikeCount(member.getId(), post.getId());
+                    latch2.countDown();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        latch2.await();
+
+        //then
+        Post postWithDecreasedLikes = postRepository.findById(post.getId()).orElseThrow(PostNotFoundException::new);
+        assertThat(postWithDecreasedLikes.getLikeCount()).isEqualTo(20); //50 - 30 = 20   (좋아요 증가 요청 - 좋아요 감소 요청)
     }
 }
