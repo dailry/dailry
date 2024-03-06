c191a181265c3c6e9afaebf2a33183ddbf610733
SSUHYUNKIM <43875264+SSUHYUNKIM@users.noreply.github.com>
2024-03-06 15:06:55 +0900

    해시태그 검색 기능 구현 (#269)
    * feat : 해시태그 검색 기능 구현
    
    * feat : 해시태그 검색 테스트 코드 작성
    
    * refactor : hashtag -> hashtags로 수정
    
    * refactor : PR 리뷰 반영 수정

--- backend/src/main/java/com/daily/daily/post/repository/PostRepositoryQuerydslImpl.java
+++ backend/src/main/java/com/daily/daily/post/repository/PostRepositoryQuerydslImpl.java
@@ -1,6 +1,7 @@
 package com.daily.daily.post.repository;
 
 import com.daily.daily.post.domain.Post;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -51,5 +52,29 @@ public class PostRepositoryQuerydslImpl implements PostRepositoryQuerydsl {
                 .where(postComment.post.id.eq(postId)).execute();
         queryFactory.delete(post)
                 .where(post.id.eq(postId)).execute();
+    }
+
+    @Override
+    public Slice<Post> findPostsByHashtag(List<String> hashtags, Pageable pageable) {
+        List<Post> posts = queryFactory
+                .selectFrom(post)
+                .leftJoin(post.postWriter, member).fetchJoin()
+                .innerJoin(post.postHashtags).fetchJoin()
+                .where(hashtags.stream()
+                        .map(tag -> post.postHashtags.any().hashtag.tagName.eq(tag))
+                        .reduce(BooleanExpression::and)
+                        .orElse(null))
+                .orderBy(post.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        boolean hasNext = posts.size() > pageable.getPageSize();
+
+        if (hasNext) {
+            posts.remove(posts.size() - 1); // 마지막 항목 제거
+        }
+
+        return new SliceImpl<>(posts, pageable, hasNext);
     }
 }
--- backend/src/test/java/com/daily/daily/post/fixture/PostFixture.java
+++ backend/src/test/java/com/daily/daily/post/fixture/PostFixture.java
@@ -4,20 +4,15 @@ import com.daily.daily.member.domain.Member;
 import com.daily.daily.post.domain.Hashtag;
 import com.daily.daily.post.domain.Post;
 import com.daily.daily.post.domain.PostHashtag;
-import com.daily.daily.post.dto.PostReadResponseDTO;
-import com.daily.daily.post.dto.PostReadSliceResponseDTO;
-import com.daily.daily.post.dto.PostWriteRequestDTO;
-import com.daily.daily.post.dto.PostWriteResponseDTO;
+import com.daily.daily.post.dto.*;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static com.daily.daily.member.fixture.MemberFixture.일반회원1;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -129,6 +124,37 @@ public class PostFixture {
                 .hasNext(true)
                 .presentPage(7)
                 .posts(List.of(게시글1, 게시글2, 게시글3, 게시글4, 게시글5))
+                .build();
+    }
+
+    public static PostReadSliceResponseDTO 게시글_해시태그로_조회_DTO() {
+        PostReadResponseDTO 게시글1 = PostReadResponseDTO.builder()
+                .postId(36L)
+                .content("오늘 다일리 어떤가요?")
+                .pageImage("https://imageURL123.com")
+                .writerId(3L)
+                .writerNickname("신입생123")
+                .hashtags(List.of("대학생", "시험기간"))
+                .createdTime(POST_CREATED_TIME)
+                .likeCount(5L)
+                .build();
+
+        PostReadResponseDTO 게시글2 = PostReadResponseDTO.builder()
+                .postId(38L)
+                .content("과제 실화냐..")
+                .pageImage("https://imageURL123.com")
+                .writerId(31L)
+                .writerNickname("다일리개발자")
+                .hashtags(List.of("대학생", "시험기간", "과제"))
+                .createdTime(POST_CREATED_TIME)
+                .likeCount(33L)
+                .build();
+
+        return PostReadSliceResponseDTO
+                .builder()
+                .hasNext(true)
+                .presentPage(7)
+                .posts(List.of(게시글1, 게시글2))
                 .build();
     }
 
