b2dd30961a7449ef8a1035b7e40e1315eaa56b04
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-03-11 19:20:22 +0900

    feat:Moveable 컴포넌트 과  타겟 꾸미기 컴포넌트 스타일 동기화 방식 변경
    * 꾸미기 컴포넌트의 style 관련 데이터 와 실제 Moveable 컴포넌트의 메서드로 인해 바뀌는 style 이 서로 다른 맥락을 가지도록 수정
       - Moveable 타겟이 변경될 때, 타겟의 스타일이 초기화 되지 않도록 수정
          - on[***]Start, on[***]End 메서드 제거
          - onRender 메서드 를 이용해 타겟 꾸미기 컴포넌트 스타일 변경
       - 공통 속성에 initialStyle 속성 추가
           - 서버에서 가져온 꾸미기 컴포넌트의 초기 스타일 값을 적용하기 위한 속성
           - 서버에서 가져온 꾸미기 컴포넌트의 데이터를 initialStyle 에 다시 할당하는 가공하는 프로세스 추가
           - 수정되는 꾸미기 컴포넌트 (canEditDecorateComponent) 의 데이터가 수정될 때 해당 속성은 변하지 않음

--- frontend/src/components/da-ily/Moveable/Moveable.jsx
+++ frontend/src/components/da-ily/Moveable/Moveable.jsx
@@ -1,25 +1,20 @@
 import Moveable from 'react-moveable';
 import PropTypes from 'prop-types';
-import MoveableHelper from 'moveable-helper';
-import { useState } from 'react';
 
 const MoveableComponent = (props) => {
   const { target, setCommonProperty } = props;
-  const [helper] = useState(() => {
-    return new MoveableHelper();
-  });
 
   return (
     <Moveable
       target={target}
       draggable={true}
       resizable={true}
-      rotatable={true}
       throttleDrag={0}
       throttleResize={0}
       throttleRotate={0}
-      onDragStart={helper.onDragStart}
-      onDrag={helper.onDrag}
+      onRender={(e) => {
+        e.target.style.cssText += e.cssText;
+      }}
       onDragEnd={(e) => {
         if (e.isDrag) {
           const movedX = parseInt(e.lastEvent.translate[0], 10);
@@ -33,24 +28,20 @@ const MoveableComponent = (props) => {
           });
         }
       }}
-      onRotateStart={helper.onRotateStart}
-      onRotate={helper.onRotate}
       onRotateEnd={(e) => {
         const transformString = e.target.style.transform;
         const deg = transformString.split('rotate(')[1].split('deg')[0];
 
         setCommonProperty({ rotation: deg });
       }}
-      onResizeStart={helper.onResizeStart}
-      onResize={helper.onResize}
-      onResizeEnd={(e) =>
+      onResizeEnd={(e) => {
         setCommonProperty({
           size: {
-            width: e.target.style.width,
-            height: e.target.style.height,
+            width: e.lastEvent.width,
+            height: e.lastEvent.height,
           },
-        })
-      }
+        });
+      }}
     />
   );
 };
--- frontend/src/pages/DailryPage/DailryPage.jsx
+++ frontend/src/pages/DailryPage/DailryPage.jsx
@@ -112,7 +112,15 @@ const DailryPage = () => {
       const page = await getPage(pageIds[pageNumber - 1]);
 
       if (page.data?.elements.length > 0) {
-        setDecorateComponents(page.data?.elements);
+        const datas = page.data?.elements.map((i) => ({
+          ...i,
+          initialStyle: {
+            position: i.position,
+            size: i.size,
+            rotation: i.rotation,
+          },
+        }));
+        setDecorateComponents(datas);
       }
     })();
   }, [pageIds, pageNumber]);
@@ -125,13 +133,6 @@ const DailryPage = () => {
       closeOnClick: true,
       transition: Zoom,
     });
-  };
-
-  const initializeMoveableStyle = () => {
-    const newStyleString = ` translate(0px, 0px) rotate(${canEditDecorateComponent?.rotation}deg) scale(1, 1) `;
-    moveableRef[target].style.transform = newStyleString;
-
-    setTarget(null);
   };
 
   const handleLeftArrowClick = () => {
@@ -171,7 +172,7 @@ const DailryPage = () => {
     if (editMode === EDIT_MODE.COMMON_PROPERTY) {
       if (canEditDecorateComponent) {
         completeModifyDecorateComponent();
-        initializeMoveableStyle();
+        setTarget(null);
 
         setCanEditDecorateComponent(null);
       }
@@ -194,7 +195,7 @@ const DailryPage = () => {
       canEditDecorateComponent.id !== element.id
     ) {
       completeModifyDecorateComponent();
-      initializeMoveableStyle();
+      setTarget(null);
       setCanEditDecorateComponent(null);
       return;
     }
@@ -213,9 +214,12 @@ const DailryPage = () => {
       canEditDecorateComponent &&
       canEditDecorateComponent.id !== element.id
     ) {
-      initializeMoveableStyle();
+      setTarget(null);
     }
   };
+  useEffect(() => {
+    console.log(canEditDecorateComponent);
+  }, [canEditDecorateComponent]);
 
   return (
     <S.FlexWrapper>
@@ -294,7 +298,7 @@ const DailryPage = () => {
               }
               if (canEditDecorateComponent) {
                 completeModifyDecorateComponent();
-                initializeMoveableStyle();
+                setTarget(null);
                 setCanEditDecorateComponent(null);
               }
               setSelectedTool(selectedTool === t ? null : t);
@@ -320,7 +324,7 @@ const DailryPage = () => {
               }
               if (canEditDecorateComponent) {
                 completeModifyDecorateComponent();
-                initializeMoveableStyle();
+                setTarget(null);
                 setCanEditDecorateComponent(null);
               }
               setSelectedTool(selectedTool === t ? null : t);
@@ -340,6 +344,7 @@ const DailryPage = () => {
               }
               if (t === 'save') {
                 const formData = getPageFormData(updatedDecorateComponents);
+                console.log(updatedDecorateComponents);
                 await patchPage(pageIds[pageNumber - 1], formData);
               }
             };
