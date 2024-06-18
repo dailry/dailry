8dcb554152aece17504382a4a1730e06e9a1c07b
gomudayya <gomudayya@gmail.com>
2024-06-18 17:46:47 +0900

    refactor : #346 CheckdException을 Wrapping 하는 클래스 작성

--- backend/src/main/java/com/daily/daily/common/exception/FileUploadFailureException.java
+++ backend/src/main/java/com/daily/daily/common/exception/FileUploadFailureException.java
@@ -1,16 +1,7 @@
 package com.daily.daily.common.exception;
 
-import java.io.IOException;
-
-public class FileUploadFailureException extends RuntimeException {
-    private final Exception originalException;
-
-    public FileUploadFailureException(IOException e) {
-        super("파일 업로드에 실패하였습니다.");
-        originalException = e;
-    }
-
-    public Exception getOriginalException() {
-        return originalException;
+public class FileUploadFailureException extends WrappingCheckedException {
+    public FileUploadFailureException(Exception checkedException) {
+        super(ErrorCode.FILE_UPLOAD_FAILURE, checkedException);
     }
 }
---
+++ backend/src/main/java/com/daily/daily/common/exception/WrappingCheckedException.java
@@ -0,0 +1,23 @@
+package com.daily.daily.common.exception;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Checked Exception을 Wrapping 해야할 때 사용합니다.
+ *
+ * ExceptionHandler를 통해 에러에 대한 API 응답을 내려주고,
+ * log로 실제 발생한 에러가 어떤것인지 확인해야 할 때 사용됩니다. Ex) FileUploadFailureException
+ */
+
+@Slf4j
+@Getter
+public class WrappingCheckedException extends CustomException {
+    private final Exception checkedException;
+    public WrappingCheckedException(ErrorCode errorCode, Exception checkedException) {
+        super(errorCode);
+        this.checkedException = checkedException;
+
+        log.error(checkedException.getMessage(), checkedException);
+    }
+}
